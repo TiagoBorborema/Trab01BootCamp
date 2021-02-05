717d3616a6a2365a711f098adf38ffd6aadafcbf
TiagoBorborema <tiagoborborema.ti@gmail.com>
2021-02-05 00:13:52 -0300

    Seed database

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into tb_client(name,cpf,income,birth_date,children)values('Tiago Borborema','098267382734',10000.00,TIMESTAMP WITH TIME ZONE '1988-02-08T20:50:07.12345Z',1);
+insert into tb_client(name,cpf,income,birth_date,children)values('José Alvares','297263704975',9300.00,TIMESTAMP WITH TIME ZONE '1945-10-11T20:50:07.12345Z',4);
+insert into tb_client(name,cpf,income,birth_date,children)values('Marcio Mendes','827304988693',5000.00,TIMESTAMP WITH TIME ZONE '2000-11-15T20:50:07.12345Z',1);
+insert into tb_client(name,cpf,income,birth_date,children)values('Adriana Santos','29374600123',2000.00,TIMESTAMP WITH TIME ZONE '1999-07-22T20:50:07.12345Z',0);
+insert into tb_client(name,cpf,income,birth_date,children)values('Rita de Cassia','24195874734',3400.00,TIMESTAMP WITH TIME ZONE '1991-01-15T20:50:07.12345Z',0);
+insert into tb_client(name,cpf,income,birth_date,children)values('Claudio Nandez','10274986854',7000.00,TIMESTAMP WITH TIME ZONE '1970-03-023T20:50:07.12345Z',2);
+insert into tb_client(name,cpf,income,birth_date,children)values('Tiago Borborema','75800155328',2500.00,TIMESTAMP WITH TIME ZONE '2001-05-30T20:50:07.12345Z',2);
+insert into tb_client(name,cpf,income,birth_date,children)values('Tiago Borborema','16258180365',8000.00,TIMESTAMP WITH TIME ZONE '1975-03-12T20:50:07.12345Z',1);
+commit;
